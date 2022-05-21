01803d063513b7c2366b78d7300b9cf198587262
Addison <ninjasmosa@vivaldi.net>
2022-05-21 10:31:56 +0100

    Update hatsune miku song chooser.py

--- hatsune miku song chooser.py
+++ hatsune miku song chooser.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-songList = ["World is Mine", "Senbonzakura", "Freely Tomorrow", "Ievan Polkka", "Magical Girl and Chocolate", "Nyanyanyanyanyanyanya", "MikuMiku Ni Shiteageru", "Viva Happy", "From Y to Y", "Love Trial", "Miracle Paint", "Summer Idol", "HIBANA", "Worlds's End Dancehall", "Hibikase", "The Disappearence of Hatsune Miku", "Tell Your World", "Brand New Day", "Ohedo Julianight", "Assassin Princess", "Melt", "Nee Nee Nee", "Ai Dee", "Newly Edgy Idols", "Maison Hatsune"]
+songList = ["World is Mine", "Senbonzakura", "Freely Tomorrow", "Ievan Polkka", "Magical Girl and Chocolate", "Nyanyanyanyanyanyanya", "MikuMiku Ni Shiteageru", "Viva Happy", "From Y to Y", "Love Trial", "Miracle Paint", "Summer Idol", "HIBANA", "Worlds's End Dancehall", "Hibikase", "The Disappearence of Hatsune Miku", "Tell Your World", "Brand New Day", "Ohedo Julianight", "Assassin Princess", "Melt", "Nee Nee Nee", "Ai Dee", "Newly Edgy Idols", "Maison Hatsune", "Teo", "The intense voice of Hatsune Miku", "Common World Domination", "39Music", "Greenlights Serenade"]
 print("Hello. You have arrived at the Hatsune Miku song chooser.")
 time.sleep(1)
 print(" ")
